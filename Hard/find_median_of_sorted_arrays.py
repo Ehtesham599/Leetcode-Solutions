a0f48622da3fa0a7c39407b138a921e02ff4d2a4
Ehtesham <ehtesham599@gmail.com>
2021-11-19 12:26:46 +0300

    Added function inside class

--- Hard/find_median_of_sorted_arrays.py
+++ Hard/find_median_of_sorted_arrays.py
@@ -1,19 +1,20 @@
 from typing import List
 
 
-def find_median_sorted_arrays(nums1: List[int], nums2: List[int]) -> float:
-    l1 = []
-    l1.extend(nums1)
-    l1.extend(nums2)
-    l1.sort()
-    n = len(l1)
-    if n % 2 == 0:
-        n1 = l1[int(n/2) - 1]
-        n2 = l1[int(n/2)]
-        if n1 or n2:
-            return (n1+n2)/2
+class Solution:
+    def find_median_sorted_arrays(nums1: List[int], nums2: List[int]) -> float:
+        l1 = []
+        l1.extend(nums1)
+        l1.extend(nums2)
+        l1.sort()
+        n = len(l1)
+        if n % 2 == 0:
+            n1 = l1[int(n/2) - 1]
+            n2 = l1[int(n/2)]
+            if n1 or n2:
+                return (n1+n2)/2
 
-        return 0
+            return 0
 
-    else:
-        return l1[int(n/2)]
+        else:
+            return l1[int(n/2)]
